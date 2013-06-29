3b9b7c58412b2c9ee341355c2169a0b5f6e60b36
Fedik <getthesite@gmail.com>
2013-06-29 12:37:38 +0300

    add type helper

---
+++ administrator/components/com_types/helpers/ucmtypehelper.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_types
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * UCM Type helper.
+ *
+ * @package     Joomla.Administrator
+ */
+class UCMTypeHelper
+{
+	/**
+	 * Configure the Submenu links.
+	 *
+	 * @param   string  The extension.
+	 *
+	 * @return  void
+	 */
+	public static function addSubmenu($extension)
+	{
+
+	}
+
+}
