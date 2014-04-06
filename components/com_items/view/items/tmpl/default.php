0a44192432c630c83728ae0e37e94954434fc5d3
Fedik <getthesite@gmail.com>
2014-04-06 16:38:17 +0300

    menu xml for items

---
+++ components/com_items/view/items/tmpl/default.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_items
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+?>
+<?php  if ($this->params->get('show_page_heading')) : ?>
+<h1>
+	<?php echo $this->escape($this->params->get('page_heading')); ?>
+</h1>
+<?php endif;  ?>
+
+
+
