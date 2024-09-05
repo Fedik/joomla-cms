dd358e8b36da9e2cdd40caa256204faa00e19649
Fedik <getthesite@gmail.com>
2024-09-05 18:22:30 +0300

    Bigdata plugin

--- plugins/sampledata/bigdata/src/Extension/BigdataPlugin.php
+++ plugins/sampledata/bigdata/src/Extension/BigdataPlugin.php
@@ -10,12 +10,9 @@
 
 namespace Joomla\Plugin\SampleData\Bigdata\Extension;
 
-use Joomla\CMS\Application\ApplicationHelper;
-use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Event\Plugin\AjaxEvent;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\Component\Categories\Administrator\Model\CategoryModel;
 use Joomla\Database\DatabaseAwareTrait;
 use Joomla\Event\SubscriberInterface;
 
@@ -39,7 +36,7 @@ final class BigdataPlugin extends CMSPlugin implements SubscriberInterface
      *
      * @since   __DEPLOY_VERSION__
      */
-    protected static $steps = 10;
+    protected static $steps = 100;
 
     /**
      * Returns an array of events this subscriber will listen to.
@@ -99,7 +96,7 @@ final class BigdataPlugin extends CMSPlugin implements SubscriberInterface
      */
     public function onAjaxSampledataApplyStep(AjaxEvent $event): void
     {
-        $app   = $event->getApplication();
+        $app   = $this->getApplication();
         $input = $app->getInput();
 
         if ($input->get('type') !== $this->_name) {
@@ -110,10 +107,176 @@ final class BigdataPlugin extends CMSPlugin implements SubscriberInterface
 
         $response            = [];
         $response['success'] = true;
-        $response['message'] = 'Step ' . $step;
+        $response['message'] = '';
 
-        sleep(1);
+        try
+        {
+            // Create a category
+            if ($step === 1) {
+                $catIds = $this->addCategories([[
+                    'title'     => 'Big data at ' . date('Y-m-d H:i:s'),
+                    'parent_id' => 1,
+                ]], 'com_content', 1);
+
+                $app->setUserState('sampledata.bigdata.catids', $catIds);
+            }
+
+            // Create an articles
+            else {
+                $amount   = 10;
+                $articles = [];
+                $catIds   = $app->getUserState('sampledata.bigdata.catids', []);
+                $catId    = reset($catIds);
+
+                if (!$catId) {
+                    throw new \UnexpectedValueException('Category ID not found');
+                }
+
+                for($i = 1; $i <= $amount; $i++) {
+                    $articles[] = [
+                        'catid' => $catId,
+                    ];
+                }
+
+                $this->addArticles($articles);
+            }
+
+            $response['message'] = 'Step ' . $step . ' finished with great success!';
+        } catch (\Throwable $e) {
+            $response['success'] = false;
+            $response['message'] = 'Step ' . $step . ' failed with error: ' . $e->getMessage();
+        }
 
         $event->addResult($response);
+    }
+
+    /**
+     * Adds categories.
+     *
+     * @param   array    $categories  Array holding the category arrays.
+     * @param   string   $extension   Name of the extension.
+     *
+     * @return  array  IDs of the inserted categories.
+     *
+     * @throws  \Exception
+     *
+     * @since  __DEPLOY_VERSION__
+     */
+    private function addCategories(array $categories, string $extension): array
+    {
+        $app      = $this->getApplication();
+        $catModel = $app->bootComponent('com_categories')->getMVCFactory()->createModel('Category', 'Administrator', ['ignore_request' => true]);
+        $catIds   = [];
+        $user     = $app->getIdentity();
+
+        foreach ($categories as $category) {
+            $category['id']              = 0;
+            $category['published']       = 1;
+            $category['access']          = 1;
+            $category['created_user_id'] = $category['created_user_id'] ?? $user->id;
+            $category['extension']       = $extension;
+            $category['level']           = $category['level'] ?? 1;
+            $category['associations']    = [];
+            $category['params']          = [];
+            $category['language']        = $category['language'] ?? '*';
+            $category['title']           = $category['title'] ?? $this->sentence();
+            $category['description']     = $category['description'] ?? str_repeat($this->text(), 10);
+
+            if (!$catModel->save($category)) {
+                throw new \Exception($catModel->getError());
+            }
+
+            // Get ID from category we just added
+            $catIds[] = $catModel->getState($catModel->getName() . '.id');
+        }
+
+        return $catIds;
+    }
+
+    /**
+     * Adds articles.
+     *
+     * @param   array  $articles  Array holding the article arrays.
+     *
+     * @return  array  IDs of the inserted categories.
+     *
+     * @throws  \Exception
+     *
+     * @since  __DEPLOY_VERSION__
+     */
+    private function addArticles(array $articles): array
+    {
+        $app        = $this->getApplication();
+        $user       = $app->getIdentity();
+        $mvcFactory = $app->bootComponent('com_content')->getMVCFactory();
+        $ids        = [];
+
+        foreach ($articles as $article) {
+            /** @var \Joomla\Component\Content\Administrator\Model\ArticleModel $model */
+            $model = $mvcFactory->createModel('Article', 'Administrator', ['ignore_request' => true]);
+
+            $article['title']     = $article['title'] ?? $this->sentence();
+            $article['introtext'] = $article['introtext'] ?? str_repeat('<p>' . $this->text() . '</p>', 10);
+            $article['fulltext']  = $article['fulltext'] ?? str_repeat('<p>' . $this->text() . '</p>', 100);
+
+            $article['id']              = 0;
+            $article['access']          = 1;
+            $article['state']           = 1;
+            $article['created_user_id'] = $article['created_user_id'] ?? $user->id;
+            $article['language']        = '*';
+            $article['associations']    = [];
+            $article['metakey']         = '';
+            $article['metadesc']        = '';
+            $article['xreference']      = '';
+            $article['featured']        = $article['featured'] ?? 0;
+
+            // Set images to empty if not set.
+            if (!empty($article['images'])) {
+                // JSON Encode it when set.
+                $article['images'] = json_encode($article['images']);
+            }
+
+            if (!$model->save($article)) {
+                $app->getLanguage()->load('com_content');
+                throw new \Exception(Text::_($model->getError()));
+            }
+
+            // Get ID from category we just added
+            $ids[] = $model->getState($model->getName() . '.id');
+        }
+
+        return $ids;
+    }
+
+    private function text($min = 30, $max = 120): string
+    {
+        $s = "Aliquam lectus nulla, eleifend ut tellus in, euismod porttitor arcu. Aliquam erat volutpat. Morbi massa sapien, condimentum ultrices pretium porta, semper ac lacus. Nulla pharetra, urna a lacinia facilisis, neque libero placerat turpis, vel tincidunt massa nunc ac nunc. In nec volutpat ligula. Sed hendrerit ligula vel felis venenatis egestas. Duis sit amet pharetra erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi rutrum vel magna et tincidunt. Curabitur mattis, sem eget suscipit porta, nibh enim auctor mi, eu lacinia magna velit eu quam. Donec commodo ultrices lorem vel ultrices. Aliquam erat volutpat. Praesent in libero bibendum, euismod dui quis, volutpat purus. Fusce non est egestas, volutpat ipsum ut, cursus augue. In hac habitasse platea dictumst. Mum volutpat.";
+        $s = explode(' ', $s);
+        shuffle($s);
+        $s = implode(' ', $s);
+        $s = ucfirst(substr($s, 0, rand($min, $max)));
+
+        return $s;
+    }
+
+    private function words($max = 5, $cmin = 2, $cmax = 6): string
+    {
+        $t = array();
+
+        for ($a = 0; $a <= ($max - 1); $a++)
+        {
+            $t[] = str_replace(array(',', '.'), array('', ''), $this->text($cmin, ($cmax + 2)));
+        }
+
+        return implode(' ', $t);
+    }
+
+    private function sentence($min = 3, $max = 10): string
+    {
+        $l = rand($min, $max);
+
+        $w = ucfirst(strtolower($this->words($l))) . '.';
+
+        return $w;
     }
 }
